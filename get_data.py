4e2d9dcf0daeef0b8b9f6f3e14224e683db671f2
AIWalaBro <aiwalbro@gmail.com>
2024-09-12 23:59:27 +0530

    get_data.py updated

--- get_data.py
+++ get_data.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import json
+import pandas as pd
+import numpy as np
+import pymongo
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,19 +12,12 @@ load_dotenv()
 MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 print(MONGO_DB_URL)
 
+
 import certifi
 ce = certifi.where()
 
-import pandas as pd
-import numpy as np
-import pymongo
-
-
 from networksecurity.exception.exception import NetworkSecurityException
-
 from networksecurity.logger.logger import logging
-
-
 
 
 class NetworkDataExtract():
@@ -32,17 +28,44 @@ class NetworkDataExtract():
             raise  NetworkSecurityException(e, sys)
         
 
-    def csv_to_json_converter(self):
+    def csv_to_json_converter(self, file_path):
         try:
-            pass
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            # convert csv data into json format
+            records = list((json.loads(data.T.to_json())).values())
+            return records
+            
         except Exception as e:
             raise  NetworkSecurityException(e, sys)
+            
         
-    def pushing_data_to_mongodb(self):
+    def pushing_data_to_mongodb(self, records, database, collection):
         try:
-            pass
+            self.records    = records
+            self.database   = database
+            self.collection = collection
+
+            # for pushing record intialise the client through pymongo
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database     = self.mongo_client[self.database]
+            self.collection   = self.database[self.collection]
+            self.collection.insert_many(self.records)
+
+            return len(self.records)
+
+
         except Exception as e:
             raise  NetworkSecurityException(e,sys)
 
+
 if __name__ == "__main__":
-    pass
+    FILE_PATH  = "./Network_Data/NetworkData.csv"
+    DATABASE   = "AIWalaBro_Database"
+    COLLECTION = "Network_Security_Collection"
+
+    # you have to create object of the class and call the function
+    network_obj = NetworkDataExtract()
+    network_json_records = network_obj.csv_to_json_converter(FILE_PATH)
+    mongodb_pusher = network_obj.pushing_data_to_mongodb(network_json_records, DATABASE, COLLECTION)
+    print(mongodb_pusher)
