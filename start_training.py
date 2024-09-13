712476333261978c043fb3c33b97eb4f5a642d3c
AIWalaBro <aiwalbro@gmail.com>
2024-09-13 16:00:26 +0530

    master training pipeline structure updated

--- start_training.py
+++ start_training.py
@@ -10,7 +10,10 @@ from networksecurity.pipeline.training_pipeline import TrainingPipeline
 # method for start training
 def start_training():
     try:
-        pass
+        # create object of Trainingpipline class and
+        # for running entire pipeline exceute run pipeline function  
+        model_training = TrainingPipeline()
+        model_training.run_pipeline()
     except Exception as e:
         raise NetworkSecurityException(e, sys)
     
