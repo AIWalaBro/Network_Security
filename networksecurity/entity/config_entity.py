85f57609a684b7edb9a0a7fc8c92b78f4cd38d61
AIWalaBro <aiwalbro@gmail.com>
2024-09-14 09:04:26 +0530

    constant(variable) value updated

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -2,7 +2,8 @@ from datetime import datetime
 import os
 from networksecurity.constant import training_pipeline
 
-print(training_pipeline.ARTIFACT_DIR)
+# print(training_pipeline.ARTIFACT_DIR)
+print(training_pipeline.FILE_NAME)
 
 class TrainingPipelineConfig:
     def __init__(self):
